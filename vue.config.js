607f35091d24fb41216a80e0cafe4a399ce31dc1
Juan Sebastian <jsterreros@hotmail.com>
2025-07-24 12:22:29 -0500

    Correcciones index y vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
+// vue.config.js
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // Configuración del publicPath para GitLab Pages
   publicPath: process.env.NODE_ENV === 'production'
       ? '/poli-market/'
       : '/'
