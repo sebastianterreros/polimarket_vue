96fa43c399f6bbd275748240589344aa1051dedb
Juan Sebastian Terreros <96632920+sebastianterreros@users.noreply.github.com>
2025-07-24 19:32:23 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  // Configura publicPath solo para producción
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/'
-      : '/'
+    // El nombre del repositorio es 'polimarket_vue', por lo tanto, la ruta base es '/polimarket_vue/'
+    ? '/polimarket_vue/'
+    : '/' // Para desarrollo local, la ruta base sigue siendo la raíz
 })
